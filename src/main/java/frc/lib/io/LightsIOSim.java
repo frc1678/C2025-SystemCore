b342e6838215e415ba202e04d70e887e7002d069
AniKashyap <anirudh.s.kashyap@gmail.com>
2025-08-06 17:55:07 -0700

    no crashes (Just sidestepped the sim stuff for now)

--- src/main/java/frc/lib/io/LightsIOSim.java
+++ src/main/java/frc/lib/io/LightsIOSim.java
@@ -1,5 +1,7 @@
 package frc.lib.io;
 
+import com.ctre.phoenix6.controls.ControlRequest;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.lib.io.LightsIO.State.RGBColor;
 
@@ -15,10 +17,15 @@ public class LightsIOSim extends LightsIO {
 	}
 
 	@Override
-	protected void setLEDs(RGBColor color, int startIndex, int numLeds) {
-		for (int i = startIndex; i < startIndex + numLeds; i++) {
-			leds[i] = color;
-			SmartDashboard.putNumberArray("LEDs Sim/" + i, new double[] {leds[i].r, leds[i].g, leds[i].b});
-		}
+	protected void setLEDs(ControlRequest color) {
+		SmartDashboard.putString("LEDs Sim/", color.getControlInfo().values().toArray().toString());
 	}
+
+	// @Override
+	// protected void setLEDs(RGBColor color, int startIndex, int numLeds) {
+	// 	for (int i = startIndex; i < startIndex + numLeds; i++) {
+	// 		leds[i] = color;
+	// 		SmartDashboard.putNumberArray("LEDs Sim/" + i, new double[] {leds[i].r, leds[i].g, leds[i].b});
+	// 	}
+	// }
 }
