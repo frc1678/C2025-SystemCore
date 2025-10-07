c064b8c641fe5357cbbcf1117e563a80ca42fd62
AniKashyap <anirudh.s.kashyap@gmail.com>
2025-10-07 16:27:54 -0700

    port update ig?

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -20,7 +20,7 @@ public enum Ports {
 
 	END_EFFECTOR_CORAL_BREAMBREAK(RobotConstants.isComp ? 1 : 5, "RioDigitalIn"),
 	END_EFFECTOR_ALGAE_BEAMBREAK(RobotConstants.isComp ? 0 : 0, "RioDigitalIn"),
-	INDEXER_BEAMBREAK(RobotConstants.isComp ? 8 : 2, "RioDigitalIn"),
+	INDEXER_BEAMBREAK(RobotConstants.isComp ? 8 : 4, "RioDigitalIn"),
 
 	ENCODER_41T(4, "canivore1"),
 	ENCODER_39T(5, "canivore1"),
